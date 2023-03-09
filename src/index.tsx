7a1570621f34700c727a89a15d276061f3d34cf2
floatgray <599316783@qq.com>
2023-03-09 12:13:07 +0800

    add routers

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ import "./assets/styles/common.scss";
 import "./assets/styles/iconfont.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
